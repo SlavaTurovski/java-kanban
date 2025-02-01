e7ab0c76d0aa1930438960ccede2bcf09a1a5f2b
SlavaTurovski <turowscky2018@gmail.com>
2025-02-01 11:47:15 +0300

    изменил механику хранения истории

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -4,27 +4,89 @@ import interfaces.HistoryManager;
 import tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private static final int LIMIT_TASKS = 10;
-    private final List<Task> historyTasks = new ArrayList<>();
+    private final HashMap<Integer, Node> history = new HashMap<>();
+    private Node head;
+    private Node tail;
+
+    private void linkLast(Task task) {
+        Node node = new Node(task, null, tail);
+
+        if (tail == null) {
+            head = node;
+        } else {
+            tail.setNext(node);
+            node.setPrev(tail);
+        }
+
+        tail = node;
+        history.put(task.getId(), node);
+    }
+
+    private ArrayList<Task> getTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        Node currentNode = head;
+
+        while (currentNode != null) {
+            tasks.add(currentNode.getData());
+            currentNode = currentNode.getNext();
+        }
+        return tasks;
+    }
+
+    private void removeNode(Node node) {
+        if (node != null) {
+            Node nextNode = node.getNext();
+            Node prevNode = node.getPrev();
+
+            if (node == head) {
+                head = nextNode;
+            }
+            if (node == tail) {
+                tail = prevNode;
+            }
+            if (prevNode != null) {
+                prevNode.setNext(nextNode);
+            }
+            if (nextNode != null) {
+                nextNode.setPrev(prevNode);
+            }
+
+            node.setNext(null);
+            node.setPrev(null);
+        }
+    }
 
     @Override
     public void add(Task task) {
-        if (task == null) {
-            System.out.println("Задача не найдена!");
-            return;
+        if (task != null) {
+            remove(task.getId());
+            linkLast(task);
         }
-        if (historyTasks.size() >= LIMIT_TASKS) {
-            historyTasks.removeFirst();
-        }
-        historyTasks.add(task);
     }
 
     @Override
-    public List<Task> getHistory() {
-        return historyTasks;
+    public void remove(int id) {
+        if (history.isEmpty()) {
+            return;
+        }
+
+        Node nodeToRemove = history.remove(id);
+        if (nodeToRemove == null) {
+            return;
+        }
+
+        removeNode(nodeToRemove);
     }
+
+
+    @Override
+    public List<Task> getHistory() {
+        return new ArrayList<>(getTasks());
+    }
+
 }
